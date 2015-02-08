cc0b2fb5b0e57699b385dcbab8d4a64726338ff8
Philipp Blume <pgbluster@gmail.com>
2015-02-08 12:10:52 -0600

    adjusted link in question show page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,7 +37,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find(params[:id])
     @question.update_attributes(question_params)
-    redirect_to question_path
+    redirect_to question_path(@question)
   end
 
   private
